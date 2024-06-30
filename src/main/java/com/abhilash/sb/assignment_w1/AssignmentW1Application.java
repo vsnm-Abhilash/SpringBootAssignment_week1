61ef09b0c39cd88724f5dffa91a161786a9b3596
Abhilash Vellanki <v.s.n.m.abhilash@gmail.com>
2024-06-30 19:19:15 +0530

    full commit

---
+++ src/main/java/com/abhilash/sb/assignment_w1/AssignmentW1Application.java
@@ -0,0 +1,14 @@
+package com.abhilash.sb.assignment_w1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentW1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentW1Application.class, args);
+		System.out.println("Im In!!");
+	}
+
+}
