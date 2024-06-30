052e43ed76e01c922c7ee3d8918bbe783be9687f
Abhilash Vellanki <v.s.n.m.abhilash@gmail.com>
2024-06-30 19:13:17 +0530

    first commit

---
+++ src/test/java/com/abhilash/sb/assignment_w1/AssignmentW1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhilash.sb.assignment_w1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentW1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
